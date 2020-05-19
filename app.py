0854a462d611877c735b4a0571561c5440294489
root <root@DESKTOP-LTOK6L2.localdomain>
2020-05-20 05:53:34 +0900

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 import sys, json
 import csv
 import model
-
+import model2
 
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
@@ -61,7 +61,26 @@ def upload_file():
             return send_from_directory(directory=uploads, filename='{}.csv'.format(filename), as_attachment=True)
     return render_template('index.html', label='Done')
 
-
+@app.route('/upload2', methods=['GET', 'POST'])
+def upload_file2():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            model2.make_table(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            uploads = os.path.join(app.config['csv_DB'])
+            return send_from_directory(directory=uploads, filename='{}.csv'.format(filename), as_attachment=True)
+    return render_template('index.html', label='Done')
 
 if __name__ == '__main__':
     #app.secret_key = 'super secret key'
